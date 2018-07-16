bfbf5d7b6f41bb2de32e454436155b4a8099c0d2
Stepan Lusnikov <endenwer@gmail.com>
2018-07-16 16:02:49 +0300

    Initial commit

---
+++ env/prod/env/android/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.android.main
+  (:require [clj-rn-example.android.core :as core]))
+
+ (core/init)
+
+
---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [clj-rn-example.ios.core :as core]))
+
+ (core/init)
+
+
